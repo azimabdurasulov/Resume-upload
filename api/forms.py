0cbbacd4294a7e002636bdf2b907f998b675ae3d
azimabdurasulov <azimjon20050604@gmail.com>
2023-07-02 14:06:15 +0500

    init

---
+++ api/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Resume
+
+class ResumeForm(forms.ModelForm):
+    class Meta:
+        model = Resume
+        fields = ['id', 'name', 'dob', 'gender', 'locality',
+            'email', 'city', 'pin', 'state', 'mobile', 'job_city', 'profile_image', 'my_file'
+        ]
